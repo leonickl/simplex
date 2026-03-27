1c833904294cc078b3a0a817ac3491390ec0f1a1
Leo Nickl <leo@leonickl.de>
2026-03-27 14:35:29 +0100

    format

--- index.php
+++ index.php
@@ -11,14 +11,18 @@ require_once 'vendor/autoload.php';
 //   V) x, y >= 0 (non-negativity constraint)
 
 // Build the Simplex-Tableau and insert as a two-dimensional array in below.
-// As a second parameter to the constructor, pass the number of variables that have to be evaluated in the solution (e. g. 2 for only x and 6 for also y).
-// The third parameter is an optional boolean that indicated is the solution should be shon step by step.
+// As a second parameter to the constructor, pass the number of variables that
+// have to be evaluated in the solution (e. g. 2 for only x and 6 for also y).
+// The third parameter is an optional boolean that indicated
+// if the solution should be shown step by step.
 
-echo json_encode((new Leo\Simplex\Simplex([
-    [ 1,  2, 1, 0, 0, 0,  1000], // constraint 1
-    [25, 10, 0, 1, 0, 0, 10000], // constraint 2
-    [ 1,  0, 0, 0, 1, 0,   700], // constraint 3
-    [ 0,  1, 0, 0, 0, 1,   500], // constraint 4
+$solutions = (new Leo\Simplex\Simplex([
+    [ 1,  2, 1, 0, 0, 0,  1000],      // constraint   I
+    [25, 10, 0, 1, 0, 0, 10000],      // constraint  II
+    [ 1,  0, 0, 0, 1, 0,   700],      // constraint III
+    [ 0,  1, 0, 0, 0, 1,   500],      // constraint  IV
     
     [-300, -200, 0, 0, 0, 1, -50000], // function to maximise
-], 3, true))->run()->solutions()), PHP_EOL;
+], 3, true))->run()->solutions();
+
+echo json_encode($solutions), PHP_EOL;
